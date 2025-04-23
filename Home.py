08eb89ee96476e2c315c2140f26de13851ccb061
Voyager9999 <shdzuo@gmail.com>
2025-04-23 17:54:30 +0200

    Add files via upload

--- Home.py
+++ Home.py
@@ -1,10 +1,4 @@
 import streamlit as st
 st.set_page_config(page_title='IR Toolkit', layout='wide')
-st.title("👋 Welcome to the IR Spectrum Assistant")
-
-st.markdown("""
-Use the left sidebar to navigate:
-- 📈 Spectrum Viewer
-- 🧠 Structure Identifier
-- 📄 PDF Peak Extractor
-""")
+st.title("👋 Welcome to the Modular IR Spectrum Toolkit")
+st.markdown("Use the left sidebar to navigate different functions.")
