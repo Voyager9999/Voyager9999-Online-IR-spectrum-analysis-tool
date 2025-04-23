6a6645e2225c256dc522b9d2180d9c88de4c16c8
Voyager9999 <shdzuo@gmail.com>
2025-04-23 23:53:10 +0200

    update

--- Home.py
+++ Home.py
@@ -1,6 +1,2 @@
-
 import streamlit as st
-
-st.set_page_config(page_title="IR Spectrum Assistant", layout="wide")
-st.title("👋 Welcome to IR Spectrum Assistant")
-st.markdown("Use the sidebar to explore spectrum analysis, structure matching, and literature extraction.")
+st.title('IR Tool')
